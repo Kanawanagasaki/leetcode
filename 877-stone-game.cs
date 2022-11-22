2cd0a06923e092aaf17418773277422bbd90e97c
Kanawanagasaki <kanawanagasaki@gmail.com>
2022-11-22 19:45:47 +0300

    stone-game

---
+++ 877-stone-game.cs
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=877 lang=csharp
+ *
+ * [877] Stone Game
+ */
+
+// @lc code=start
+public class Solution
+{
+    public bool StoneGame(int[] piles) => true;
+}
+// @lc code=end
+
