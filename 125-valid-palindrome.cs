af8f6d53f820694426fe74593cfc2102809b90e8
Kanawanagasaki <kanawanagasaki@gmail.com>
2022-12-25 22:24:25 +0300

    Revert "convert-sorted-array-to-binary-search-tree"
    This reverts commit ae9f1f990a3542740db39d13e72acc5c836a6dfe.

--- 125-valid-palindrome.cs
+++
@@ -1,8 +0,0 @@
-public class Solution
-{
-    public bool IsPalindrome(string s)
-    {
-        s = new string(s.Where(x => char.IsLetterOrDigit(x)).ToArray()).ToLower();
-        return s == new string(s.Reverse().ToArray());
-    }
-}
