5162c52fec09a68917e1bb1edc766e4ac1550207
Kanawanagasaki <kanawanagasaki@gmail.com>
2022-12-29 07:21:48 +0300

    2515

---
+++ 2515-shortest-distance-to-target-string-in-a-circular-array.cs
@@ -0,0 +1,18 @@
+public class Solution
+{
+    public int ClosetTarget(string[] words, string target, int startIndex)
+    {
+        var ret = int.MaxValue;
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i] != target)
+                continue;
+            var dist1 = Math.Abs(startIndex - i);
+            var dist2 = Math.Abs(Math.Abs(startIndex - i) - words.Length);
+            ret = Math.Min(ret, Math.Min(dist1, dist2));
+        }
+
+        return ret == int.MaxValue ? -1 : ret;
+    }
+}
